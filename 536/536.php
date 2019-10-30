e925a69f041f9622cfa6c21a2fd795972ab7bc48
phamxuanphu278 <phuphamxuan278@gmail.com>
2019-10-30 20:34:42 +0700

    change module 536

--- 536/536.php
+++ 536/536.php
@@ -15,18 +15,16 @@ if (!class_exists('lessc')) {
 $less = new lessc;
 $less->compileFile('less/536.less', 'css/536.css');
 ?>
-<html lang="vi" xmlns="http://www.w3.org/1999/xhtml">
+<html lang="en">
 <head>
     <title>Title - 536</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Basic usage demo">
-    <meta name="author" content="David Deutsch">
     <link href="<?php echo $url_path ?>/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
     <link href="<?php echo $url_path ?>/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="<?php echo $url_path ?>/css/536.css" rel="stylesheet" type="text/css"/>
     <script src="<?php echo $url_path ?>/js/jquery-2.1.4.min.js" ></script>
     <script src="<?php echo $url_path ?>/js/536.js"></script>
-    <link href="<?php echo $url_path ?>/css/536.css" rel="stylesheet" type="text/css"/>
 </head>
 <body>
     <?php include './536-content.php'; ?>
